20eba0e5df3b3936470c57ca52f6f33a40dbb389
Kakyoin <529106896@qq.com>
2022-02-08 05:38:55 +0800

    添加翻页

--- js/main.js
+++ js/main.js
@@ -5,19 +5,73 @@ var app = new Vue({
         musicList: [],
         musicUrl: "",
         musicCover: "",
-        musicCoverName: ""
+        musicCoverName: "",
+        searchHasMore: false,
+        searchMaxPage: 0,
+        currentPage: 0,
+        currentOffSet: 0,
+        searchResultCount: 0
     },
     methods: {
         searchMusic: function() {
             var tmp = this;
-            axios.get("https://music-api.heheda.top/search?keywords=" + this.query).then(
+            if (tmp.query == "") {
+                alert("不能输入空字符串！");
+                tmp.musicList = [];
+                //return;
+            }
+            axios.get("https://music-api.heheda.top/search?limit=10&keywords=" + this.query).then(
                 function(response) {
                     //console.log(response);
-                    tmp.musicList = response.data.result.songs;
+                    if (response.data.result.songCount == 0) {
+                        alert("无查询结果！");
+                        tmp.musicList = [];
+                    } else {
+                        tmp.musicList = response.data.result.songs;
+                        tmp.searchMaxPage = Math.ceil(response.data.result.songCount/10);
+                        tmp.searchResultCount = response.data.result.songCount;
+                        //console.log(tmp.searchMaxPage);
+                    }
+                    if (response.data.result.hasMore == true) {
+                        tmp.searchHasMore = true;
+                    } else {
+                        tmp.searchHasMore = false;
+                    }
                     tmp.musicUrl = "";
                     tmp.musicCover = "";
                     tmp.musicCoverName = "";
                     //console.log(tmp.musicList);
+                },
+                function(err) {
+                    console.log(err);
+                }
+            )
+        },
+        prevPage: function() {
+            var tmp = this;
+            tmp.currentOffSet -= 10;
+            axios.get("https://music-api.heheda.top/search?limit=10&keywords=" + tmp.query + "&offset=" + tmp.currentOffSet).then(
+                function(response) {
+                    //console.log(tmp.pageOffSet);
+                    tmp.currentPage--;
+                    //console.log(tmp.currentPage);
+                    //console.log(response);
+                    tmp.musicList = response.data.result.songs;
+                },
+                function(err) {
+                    console.log(err);
+                }
+            )
+        },
+        nextPage: function() {
+            var tmp = this;
+            tmp.currentOffSet += 10;
+            axios.get("https://music-api.heheda.top/search?limit=10&keywords=" + tmp.query + "&offset=" + tmp.currentOffSet).then(
+                function(response) {
+                    //console.log(tmp.pageOffSet);
+                    tmp.currentPage++;
+                    //console.log(tmp.currentPage);
+                    tmp.musicList = response.data.result.songs;
                 },
                 function(err) {
                     console.log(err);
